c6a398e4c012c7df962a12fcf9204e04321b3593
Juan Pablo Masyk <juanpablomasyk@gmail.com>
2022-11-28 21:37:01 -0300

    Termine el tp5

---
+++ src/components/Navigator.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "src-componentes-navigator-vue",
+  props: ['mensaje','termino'],
+
+  mounted() {
+  },
+
+  data() {
+    return {
+      isHardNav: true,      
+      colorCount:6,                  
+    };
+  },
+  methods: {
+    restart() {
+        this.$emit("restart");
+    },
+    changeHard() {
+      if (!this.isHardNav) {
+        this.isHardNav = true;              
+        this.colorCount=6
+        this.$emit("is-hard-navigator", this.isHardNav);
+        this.$emit("color-count", this.colorCount);
+        this.$emit("restart");
+      }
+    },
+    changeEasy() {
+      if (this.isHardNav) {
+        this.isHardNav = false;              
+        this.colorCount=3
+        this.$emit("is-hard-navigator", this.isHardNav);
+        this.$emit("color-count", this.colorCount);
+        this.$emit("restart");
+      }
+    },    
+  },
+  computed: {},
+};
+</script>
+
+<style scoped lang="css" >
+.src-componentes-navigator-vue {
+}
+
+</style>
